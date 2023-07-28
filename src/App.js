f74056e5a1b21a0e39fdd4a45843c7974b1176a0
eric.wenk <eric.wenk@365farmnet.com>
2023-07-28 15:01:20 +0200

    added search function

--- src/App.js
+++ src/App.js
@@ -5,27 +5,47 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            monsters: []
+            monsters: [],
+            searchField: ''
         };
     }
 
     componentDidMount() {
         fetch('http://jsonplaceholder.typicode.com/users')
             .then(response => response.json())
-            .then(users => this.setState({monsters:users}))
+            .then(users => this.setState({monsters: users}))
     }
 
     render() {
+        const {monsters, searchField} = this.state
+        const {onSearchChange} = this
+
+        const filteredMonsters = monsters.filter((element) =>
+            element.name.toLowerCase().includes(searchField))
+
+
         return (
             <div className="App">
+                <input className='searchBox'
+                       type='search'
+                       placeholder='search monsters'
+                       onChange={onSearchChange}/>
                 {
-                    this.state.monsters.map((monster) => {
+                    filteredMonsters.map((monster) => {
                         return <div key={monster.id}><h1>{monster.name}</h1></div>
                     })
                 }
             </div>
         );
     }
+
+    onSearchChange = (event) => {
+        const searchField = event.target.value.toLocaleLowerCase()
+        this.setState(() => {
+            return {searchField};
+        })
+    }
+
 }
 
 export default App;
